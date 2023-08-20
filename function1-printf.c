7f591b283575da5188727a3a9558962179940192
Roaa2222 <me@example.com>
2023-08-20 15:03:13 -0700

    code

--- function1-printf.c
+++ function1-printf.c
@@ -1,46 +1,46 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stdio.h>
 /**
  *  _printf - by printf print c , s and %
+ *
  * @format: accept specifer whether s or c or %
+ *
  * Return: Always 0
-*/
+ */
+
 int _printf(const char *format, ...)
 {
-int noofcharacters = 0;
 va_list lis;
+int noofcharacters = 0;
 
-if (format == 0)
-{
-	return (-1) ; }
 va_start(lis, format);
+if (format == 0 || (format[0] == '%' && !format[1]))
+	return (-1);
 while (*format)
 {
 	if (*format != '%')
 	{
 		write(1, format, 1);
-	noofcharacters++; }
+		noofcharacters++;
+	}
 	else
 	{
 		format++;
 		if (*format == '\0')
 			break;
-		if (*format == 'i')
+
+		if (*format == 'i' || *format == 'd')
 		{
 			int intprint = va_arg(lis, int);
+			char bufferforint[20];
+			int lenghtint = sprintf(bufferforint, "%d", intprint);
+			write(1, bufferforint, lenghtint);
 
-			write(1, &intprint, 1);
-			noofcharacters++; }
-		else if (*format == 'd')
-		{
-			int dprintf = va_arg(lis, signed int);
-				write(1, &dprintf, 1);
-			noofcharacters++; }
-		else if (*format == '%')
-		{
-			write(1, format, 1);
-			noofcharacters++; }
+			noofcharacters += lenghtint;
+		}
 	}
+	format++;
 }
 va_end(lis);
 return (noofcharacters);
