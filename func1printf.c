9758f9014b90ec9b0d3b532dd1f7954ccd926649
Tokasaker <tokasakr88@gmail.com>
2023-08-22 13:09:35 -0700

    tokaroaa

--- func1printf.c
+++ func1printf.c
@@ -1,54 +1,51 @@
+#include <unistd.h>
+#include <stdarg.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
-
 /**
  *  _printf - custom printf function to print characters, strings, and %
- *
  * @format: format specifier string
- *
  * Return: number of characters printed, or -1 on error
  */
 int _printf(const char *format, ...)
 {
-	int noofcharacters = 0, stringlength = 0;
-	va_list lis;
-
 	if (format == NULL)
-	{
-		return (-1); }
+		return (0);
+	va_list lis;
+	int noofchar = 0;
+
 	va_start(lis, format);
 	while (*format)
 	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			noofcharacters++; }
-		else
-		{
-			format++;
-			if (*format == '\0')
-				break;
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				noofcharacters++; }
-			else if (*format == 'c')
-			{
-				char character = va_arg(lis, int);
-
-				write(1, &character, 1);
-				noofcharacters++; }
-			else if (*format == 's')
+		if (*format == '%')
+		{ format++;
+			if (*format == 's')
 			{
 				char *stringprint = va_arg(lis, char*);
 
-				while (stringprint[stringlength] != '\0')
+				if (stringprint != NULL)
 				{
-					stringlength++; }
-				write(1, stringprint, stringlength);
-				noofcharacters += stringlength; } }
+					int lengthstr = strlen(stringprint);
+
+					write(1, stringprint, lengthstr);
+					noofchar += lengthstr; }
+				else
+				{
+					const char *nullString = "(null)";
+					int lengthnu = strlen(nullString);
+
+					write(1, nullString, lengthnu);
+					noofchar += lengthnu; } }
+			else if (*format == 'c')
+			{
+				int character = va_arg(lis, int);
+
+				write(1, &character, 1);
+				noofchar++; } }
+		else
+		{
+			write(1, format, 1);
+			noofchar++; }
 		format++; }
 	va_end(lis);
-	return (noofcharacters); }
+	return (noofchar); }
